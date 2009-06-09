170b294b15f90f797693b9dbdd310ac8899abc73
Axel Liljencrantz <axel.liljencrantz@freecode.no>
2009-06-09 15:27:11 +0200

    Add .gitignore, remove a bunch of crap

--- config.php
+++
@@ -1,4 +0,0 @@
-<?php
-define ('EGS_DSN', 'pgsql:dbname=egs;host=localhost;user=egs;password=8il_a9j#');
-define('DB_DSN', 'pgsql:dbname=freecode;host=localhost;user=freecode;password=SmurfeCode');
-?>
