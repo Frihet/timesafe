22423410565a55d27397aacce3c259c4f1ef9e3e
Axel Liljencrantz <axel.liljencrantz@freecode.no>
2009-06-25 10:30:14 +0200

    Don't insert fake projects in schema

--- static/schema.sql
+++ static/schema.sql
@@ -12,15 +12,15 @@ create table tr_project
 	egs_id int,
 	start_date date,
 	external boolean not null default false,
-	name varchar(64) not null,
+	name varchar(256) not null,
 	open boolean not null default true
 );
 
 create table tr_user
 (
 	id serial not null primary key,
-	name varchar(64) not null,
-	fullname varchar(128) not null,
+	name varchar(256) not null,
+	fullname varchar(256) not null,
 	deleted boolean not null default false
 );
 	
@@ -39,20 +39,20 @@ create table tr_entry
 	user_id int not null references tr_user(id),
 	minutes int not null,
 	perform_date date not null,
-	description varchar(16000) not null
+	description varchar(65536) not null
 );
 
 create table tr_tag_group
 (
 	id serial not null primary key,
-	name varchar(64) not null,
+	name varchar(256) not null,
 	required boolean not null default false
 );	
 
 create table tr_tag
 (
 	id serial not null primary key,
-	name varchar(64) not null,
+	name varchar(256) not null,
 	visibility int not null,
 	project_id int references tr_project(id),
 	group_id int references tr_tag_group(id),
@@ -68,27 +68,6 @@ create table tr_tag_map
 );
 
 insert into tr_user(name, fullname) values ('nooslilaxe','Axel Liljencrantz');
-
-insert into tr_project (name) values ('Div7 - Executive'); 
-insert into tr_project (name) values ('Div1 - Admin');
-insert into tr_project (name) values ('Div2 - Marketing and Sales');
-insert into tr_project (name) values ('Div3 - Treasury');
-insert into tr_project (name) values ('Div4 - Delivery');
-insert into tr_project (name) values ('Div5 - Quality'); 
-insert into tr_project (name) values ('Div6 - Ideology'); 
-insert into tr_project (name) values ('Div4A:Doozerland');
-insert into tr_project (name) values ('Div4G:Curriculum');
-insert into tr_project (name) values ('Elkjøp:Greencycle');
-insert into tr_project (name) values ('Elkjøp:ESC3');	
-insert into tr_project (name) values ('Elkjøp:ELOG');	
-insert into tr_project (name) values ('Elkjøp:Ecco');
-insert into tr_project (name) values ('Elkjøp:Ecco Statistics module');
-insert into tr_project (name) values ('Steen&Strøm:MAC police');
-insert into tr_project (name) values ('Ericsson:TLA');
-insert into tr_project (name) values ('ColorLine:NBI');
-insert into tr_project (name) values ('RealTech:Support');
-insert into tr_project (name) values ('Zamboni:Triumf');
-
 
 insert into tr_tag(name, visibility) values ('Billable',0);
 insert into tr_tag(name, visibility) values ('40 % overtime',0);
