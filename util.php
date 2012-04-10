7ae9ab504be50307b77a360ef9db9d9891aa53ea
Egil Moeller <egil.moller@freecode.no>
2012-04-10 12:51:21 +0200

    Date cleanup

---
+++ util.php
@@ -0,0 +1,35 @@
+<?php
+
+function makeDateSelector($name, $value, $id=null, $class=null, $attributes = array()) 
+{
+    $attr = '';
+    foreach($attributes as $key => $val) {
+        $val = htmlEncode($val);
+        $attr .= "$key='$val' ";
+    }
+    $id_str = $id?'id="'.htmlEncode($id).'"':'';
+    if ($class == null) $class = "";
+    $class_str = 'class="datepickerinput '.htmlEncode($class).'"';
+    return "<input type='text' $id_str $class_str size='16' name='".htmlEncode($name)."' value='".htmlEncode($value)."' {$attr} />\n";
+}
+
+
+function formatDate($tm)
+{
+    return date('Y-m-d', $tm);
+}
+
+function today()
+{
+    $now = time();
+    $year = date('Y', $now);
+    $month = date('m', $now);
+    $day = date('d', $now);
+    return mktime(12, 0, 0, $month, $day, $year);
+}
+
+function parseDate($date)
+{
+    list($year, $month, $day) = explode('-',$date);
+    return mktime(12, 0, 0, $month, $day, $year);
+}
